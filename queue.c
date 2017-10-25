a52aae404209929a3dfe4dab2fb90615dd4facd0
sys1036 <bharathi.raja@way2online.co.in>
2017-10-25 10:19:12 +0530

    No major changes

--- queue.c
+++ queue.c
@@ -2,74 +2,74 @@
 #include <stdlib.h>
 
 struct node {
-	int data;
-	struct node *next;
+    int data;
+    struct node *next;
 };
 
 struct node *head = NULL;
 
-void listQueue(struct node * headRef){
-	printf("Queue :: ");
-	while(headRef != NULL) {
-		printf("%d  ",headRef->data);
-		headRef=headRef->next;
-	}
-	printf("\n");
+void listQueue(struct node *headRef) {
+    printf("Queue :: ");
+    while (headRef != NULL) {
+        printf("%d  ", headRef->data);
+        headRef = headRef->next;
+    }
+    printf("\n");
 }
 
-void enqueue(int data){
-	struct node *newNode = (struct node *) malloc(sizeof(struct node));
-	newNode->data = data;
-	newNode->next = head;
-	head = newNode;
-	listQueue(head);
+void enqueue(int data) {
+    struct node *newNode = (struct node *) malloc(sizeof(struct node));
+    newNode->data = data;
+    newNode->next = head;
+    head = newNode;
+    listQueue(head);
 }
 
-void dequeue(){
-	struct node *temp = head;
-	struct node *x = NULL;
+void dequeue() {
+    struct node *temp = head;
+    struct node *x = NULL;
 
-	if(temp == NULL){
-		printf("Queue is Empty\n");
-		return;
-	}
-	if(temp->next == NULL){	
-		head = NULL;
-		free(temp);
-		printf("Queue is Empty now\n");
-		return;
-	}
-	while(temp->next != NULL){
-		x = temp;
-		temp = temp->next;
-	}
+    if (temp == NULL) {
+        printf("Queue is Empty\n");
+        return;
+    }
+    if (temp->next == NULL) {
+        head = NULL;
+        free(temp);
+        printf("Queue is Empty now\n");
+        return;
+    }
+    while (temp->next != NULL) {
+        x = temp;
+        temp = temp->next;
+    }
 
-	x->next = NULL;
-	free(temp);
+    x->next = NULL;
+    free(temp);
 
-	listQueue(head);
+    listQueue(head);
 }
 
-int main(){
-	printf("Choose an operation for queue ::\n\t\t1-Enqueue\n\t\t2-Dequeue\n\t\t'q'-quit\n");	
-	while(1){
-		char choice;
-		int e;
-		scanf("%c", &choice);	
-		switch(choice){
-			case '1': {
-				printf("enter an element to push :: ");
-				scanf("%d",&e);
-				enqueue(e);
-				break;
-			}
-			case '2':{
-				dequeue();
-				break;
-			}
-			case 'q':{
-				exit(0);	
-			}
-		}
-	}	
+int main() {
+    printf("Choose an operation for queue ::\n\t\t1-Enqueue\n\t\t2-Dequeue\n\t\t'q'-quit\n");
+    while (1) {
+        char choice;
+        int e;
+        scanf("%c", &choice);
+        switch (choice) {
+            case '1': {
+                printf("enter an element to push :: ");
+                scanf("%d", &e);
+                enqueue(e);
+                break;
+            }
+            case '2': {
+                dequeue();
+                break;
+            }
+            case 'q': {
+                exit(0);
+            }
+        }
+    }
 }
--- stack.c
+++ stack.c
@@ -1,63 +1,65 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct node{
-	int data;
-	struct node * next;
+struct node {
+    int data;
+    struct node *next;
 };
 
 struct node *head = NULL;
 
-void show(struct node * headRef) {
-	printf("Stack :: ");
-	while(headRef != NULL) {
-		printf("%d  ",headRef->data);
-		headRef=headRef->next;
-	}
-	printf("\n");
+void show(struct node *headRef) {
+    printf("Stack :: ");
+    while (headRef != NULL) {
+        printf("%d  ", headRef->data);
+        headRef = headRef->next;
+    }
+    printf("\n");
 }
 
-void push(int newData){
-	struct node *newNode = (struct node *) malloc (sizeof(struct node));
-	newNode->data = newData;
-	newNode->next = head;
-	head = newNode; 
-	show(head);
+void push(int newData) {
+    struct node *newNode = (struct node *) malloc(sizeof(struct node));
+    newNode->data = newData;
+    newNode->next = head;
+    head = newNode;
+    show(head);
 }
 
 void pop() {
-	struct node *temp = head;
-	if(temp != NULL){
-		head = temp->next;
-		free(temp);
-		show(head);
-		return;
-	}
-	else{
-		printf("\rStack is empty\n");
-	}
+    struct node *temp = head;
+    if (temp != NULL) {
+        head = temp->next;
+        free(temp);
+        show(head);
+        return;
+    } else {
+        printf("\rStack is empty\n");
+    }
 }
 
-int main(){
-	while(1){
-		char choice;
-		int e;
-		printf("Choose an operation for stack ::\n1-push\n2-pop\'q'-quit\n");
-		scanf("%c", &choice);	
-		switch(choice){
-			case '1': {
-				printf("enter an element to push :: ");
-				scanf("%d",&e);
-				push(e);
-				break;
-			}
-			case '2':{
-				pop();
-				break;
-			}
-			case 'q':{
-				exit(0);	
-			}
-		}
-	}	
+int main() {
+    char choice;
+    int e;
+    while (1) {
+        printf("\n");
+        printf("Choose an operation for stack ::\n1-push\n2-pop\nq-quit\n");
+        scanf("%c", &choice);
+        switch (choice) {
+            case '1': {
+                printf("enter an element to push :: ");
+                scanf("%d", &e);
+                push(e);
+                break;
+            }
+            case '2': {
+                pop();
+                break;
+            }
+            case 'q': {
+                exit(0);
+            }
+            default:
+                break;
+        }
+    }
 }
